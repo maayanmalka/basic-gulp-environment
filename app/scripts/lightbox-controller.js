f251263a6f954e43368f7b3a4aba9385f2e82835
maayan malka <maayanmalka@gmail.com>
2016-11-20 17:37:10 +0200

    added FREE lightbox functionality

---
+++ app/scripts/lightbox-controller.js
@@ -0,0 +1,21 @@
+//lightbox-controller.js
+
+
+$(document).ready(function() {
+	// init();
+});
+
+
+lightboxShow = function (){
+  $('.lightbox').fadeIn('fast')
+}
+
+lightboxClose = function () {
+	$('.lightbox').fadeOut('fast')
+}
+
+
+
+startDownload = function () {
+	
+}
