76298c90f2940f1855c5383da8fb611a2bf4b200
maayan malka <maayanmalka@gmail.com>
2016-11-23 14:22:11 +0200

    updated lightbox flow - PDF opens in a new tab, filename is taken from pagename

--- app/scripts/lightbox-controller.js
+++ app/scripts/lightbox-controller.js
@@ -1,10 +1,30 @@
 //lightbox-controller.js
-
-
+var downloadInterval;
+var path;
+var pageName;
+var fileName; 
+// var waitingTime = 2000;
 $(document).ready(function() {
 	// init();
 });
 
+lightboxInit = function(){
+	lightboxShow();
+	downloadPDF()
+	// downloadInterval = setInterval( downloadPDF , waitingTime);
+}
+
+
+downloadPDF = function (){
+	path = window.location.pathname;
+	pageName = path.split("/").pop();
+	fileName = pageName.replace(pageName.split(".").pop(), "pdf")
+	window.open("../pdf/" + fileName)
+
+	// window.location.assign("../pdf/" + fileName , 'Download');
+	// window.open("../pdf/" + fileName, 'Download')
+	// clearInterval(downloadInterval);
+}
 
 lightboxShow = function (){
   $('.lightbox').fadeIn('fast')
@@ -14,3 +34,6 @@ lightboxClose = function () {
 	$('.lightbox').fadeOut('fast')
 }
 
+
+// download 
+// href='../pdf/5-coloring-pages_The-Catbears.pdf' download = "5-coloring-pages_The-Catbears.pdf"
