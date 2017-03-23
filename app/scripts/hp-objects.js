d4c0f61ad3e873c51a4f59f96a8bb38dee9b93fd
maayan malka <maayanmalka@gmail.com>
2017-03-23 18:32:20 +0200

    HP POSTER - fade obj gallery added

---
+++ app/scripts/hp-objects.js
@@ -0,0 +1,37 @@
+
+var duck; 
+var fox;
+var card;
+var makerKit;
+
+var objects = [];
+var objectsTotal;
+
+var posterObjects;
+var delayInt;
+var showObjNum;
+
+$(document).ready(function(){
+
+	// SETTING VARS
+	duck = 'object__duck.png'; 
+	fox = 'object__fox.png';
+	card = 'object__card.png';
+	makerKit = 'object__maker-kit.png';
+	objects = [duck, fox, card, makerKit];
+	objectsTotal = objects.length;
+	posterObjects = $('.fullscreen__object--img');
+	showObjNum = 1;
+
+	setInterval('cycleImages()', 5000);
+
+});
+
+function cycleImages(){
+  posterObjects.css("margin-top" , '-10px' ).css('z-index' , '1').fadeOut(300, function(){
+  	posterObjects.css("background-image" , ['url("../img/hp/' + objects[showObjNum] + '")']);
+  	posterObjects.css('z-index' , '1').fadeIn(300).css("margin-top" , '0px' );
+  	showObjNum ++;
+  	showObjNum = showObjNum % objectsTotal;
+  })
+}
